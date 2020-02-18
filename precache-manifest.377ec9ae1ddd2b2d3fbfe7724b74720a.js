340bebfb8978d0163948ade6075cba47fa1bd2d1
mjbryan <mjbyran10@gmail.com>
2020-02-18 09:08:17 +0100

    Updates

---
+++ precache-manifest.377ec9ae1ddd2b2d3fbfe7724b74720a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cbf2b8c53c3de0e04241b14a0e0dbc",
+    "url": "/bookstore-app/index.html"
+  },
+  {
+    "revision": "cbda7900c8df2e43313d",
+    "url": "/bookstore-app/static/css/main.61f61604.chunk.css"
+  },
+  {
+    "revision": "2f277e1b80db080f9176",
+    "url": "/bookstore-app/static/js/2.4e5d21e2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bookstore-app/static/js/2.4e5d21e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbda7900c8df2e43313d",
+    "url": "/bookstore-app/static/js/main.5e3ba93e.chunk.js"
+  },
+  {
+    "revision": "9fe0b9af75d34f69ad71",
+    "url": "/bookstore-app/static/js/runtime-main.2e36fbb7.js"
+  }
+]);
